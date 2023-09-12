cf0a4bf5b11f16a1b2694d61ee5013496fb47d02
lyndha <ihuomalyndha@gmail.com>
2023-09-12 06:58:32 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-function tat read a filename
+function that reads a text file
 """
 
 
 def read_file(filename=""):
-    """read filename"""
-    with open('filename', "r", encoding="utf-8") as f:
-        read_data = f.read()
-         print(f.read(), end="")
+    """reads a text file (UTF8) and prints it to stdout
+    Returns none
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
