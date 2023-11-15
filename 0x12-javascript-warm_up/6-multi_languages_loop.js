7659fc533bef5cb2db578432f1b0461681456980
ihuomalyndha <ihuomalyndha@gmail.com>
2023-11-15 02:21:48 +0100

    multi lang

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -6,7 +6,7 @@ for (let arr = 0; arr < myVar.length; arr++) {
   console.log(myVar[arr]);
 }
 */
-const arr = ['C is fun', 'Python is cool', 'JavaScript'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i of arr) {
   console.log(i);
 }
