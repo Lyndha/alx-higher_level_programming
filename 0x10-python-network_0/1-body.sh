61aa8cc48c6ed35ad081732fb1b19496ba455ac7
ihuomalyndha <ihuomalyndha@gmail.com>
2023-12-01 01:49:52 +0100

    body

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# It sends a GET request to the URL, and displays the response's body
+curl -sL "$1"
+#!/bin/bash
+# It sends a GET request to the URL, and displays the response's body
+curl -sL "$1"
