e5e605cf5f7995af14d802664baf861cf2d15c3d
lyndha <ihuomalyndha@gmail.com>
2024-01-20 10:59:35 +0100

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*Write a script that searches the second biggest integer in the list of arguments.
+
+You can assume all arguments can be converted to integer
+If no argument passed, print 0
+If the number of arguments is 1, print 0
+You must use console.log(...) to print all output
+You are not allowed to use var
+*/
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
+}
