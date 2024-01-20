81664a509af281c2b3a5e8972eacfaf960825ed7
lyndha <ihuomalyndha@gmail.com>
2024-01-20 10:21:28 +0100

    semistandard --fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* Write a script that prints x times “C is fun”
  * Where x is the first argument of the script
- * If the first argument can’t be converted to an integer, 
+ * If the first argument can’t be converted to an integer,
  * print “Missing number of occurrences”
  * You must use console.log(...) to print all output
  * You are not allowed to use var
@@ -11,17 +11,15 @@
 
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   const x = Number(process.argv[2]);
-  for (i = 0; i < x; i++){
-	console.log('C is fun');
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
-
-/*	let i = 0;
+/* let i = 0;
   while (i < x) {
     console.log('C is fun');
     i++;
   }
-}*/
+} */
